6e21c8a545d2b5b3825e1b53c3d5f7633e761aa2
ASH0DANNY <115711791+ASH0DANNY@users.noreply.github.com>
2025-08-20 12:13:59 +0000

    env added, working

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,16 +1,9 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
-  api_key: import.meta.env.VITE_CLOUDINARY_API_KEY,
-  api_secret: import.meta.env.VITE_CLOUDINARY_API_SECRET,
-});
-
 export const uploadImage = async (file: File): Promise<string> => {
   try {
     const formData = new FormData();
     formData.append('file', file);
-    formData.append('upload_preset', 'puja-ecom');
+    formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
+    formData.append('cloud_name', import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
 
     const response = await fetch(
       `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
